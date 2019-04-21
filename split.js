add08e20f72e948108421eb315c17aa9500f89e5
clement <clement.vercucque@orange.fr>
2019-04-21 16:23:46 +0200

    split files by lines

--- split.js
+++ split.js
@@ -1,14 +1,13 @@
 const csvSplitStream = require('csv-split-stream')
 const fs = require('fs')
+
 const path = './splitfiles'
 const csv = '/home/clement/Bureau/BigData/data.csv'
-var pm2 = require('pm2')
 
-// verify if task folder exist
 const fileExist = () => {
   return new Promise((resolve) => {
     if (! fs.existsSync(path)) {
-      fs.mkdirSync(path);
+      fs.mkdirSync(path)
       resolve('NOT EXIST')
     } else {
       resolve('EXIST')
@@ -17,24 +16,21 @@ const fileExist = () => {
 }
 
 const splitSVG = (csv) => {
-  return new Promise((reoslve)=> {
+  return new Promise((resolve) => {
     csvSplitStream.split(
       fs.createReadStream(csv),
       {
         lineLimit: 10000
       },
-      (index) => fs.createWriteStream(`./splitfiles/data-${index}.csv`)
+      (index) => fs.createWriteStream(`./splitfiles/${index}.csv`)
     ).then((response) => {
       console.log(response)
     })
   })
 }
 
-// EXECUTION
 fileExist().then((response) => {
-  //SVG SPLIT
   splitSVG(csv).then((resolve) => {
     console.log(response)
-
   })
 })
